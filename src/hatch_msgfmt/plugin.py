2303fbfb2461ed5dad8366766285eda30a663d9a
Taylor C. Richberger <taywee@gmx.com>
2023-01-05 12:29:34 -0700

    remove debugging information

--- src/hatch_msgfmt/plugin.py
+++ src/hatch_msgfmt/plugin.py
@@ -1,5 +1,4 @@
-from typing import Any, Union
-
+from typing import Any, Union, Mapping
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from typing import BinaryIO
 from tempfile import mkstemp
@@ -38,12 +37,11 @@ class MsgfmtBuildHook(BuildHookInterface):
         super().__init__(*args, **kwargs)
         self.__files = []
 
-    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+    def initialize(self, version: str, build_data: Mapping[str, Any]) -> None:
         if self.target_name != 'wheel':
             return
 
         for included in self.build_config.builder.recurse_included_files():
-            path = Path(included.path)
             distribution_path = Path(included.distribution_path)
             if distribution_path.suffix == '.po':
                 output = UnopenedTemporaryFile(suffix='.mo')
@@ -52,21 +50,4 @@ class MsgfmtBuildHook(BuildHookInterface):
 
                 self.__files.append(output)
                 build_data['force_include'][output.name] = str(distribution_path.with_suffix('.mo'))
-                path.unlink()
-
-        print(f'{[(file.path, file.relative_path, file.distribution_path) for file in  self.build_config.builder.recurse_included_files()]=}')
-
-    def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str) -> None:
-        if self.target_name != 'wheel':
-            return
-        print('finalize')
-        print(f'{version=}')
-        print(f'{build_data=}')
-        print(f'{artifact_path=}')
-        print(f'{self.directory=}')
-        print(f'{self.build_config.directory=}')
-        print(f'{self.build_config.default_include()=}')
-        print(f'{self.build_config.default_exclude()=}')
-        print(f'{self.build_config.default_only_include()=}')
-        print(f'{self.build_config.default_packages()=}')
-        print(f'{[(file.path, file.relative_path, file.distribution_path) for file in  self.build_config.builder.recurse_included_files()]=}')
+                os.remove(included.path)
