129cd0b15a63bc844a46b5bb79553262c7faba26
Adrián Chaves <adrian@chaves.io>
2023-04-07 12:03:44 +0200

    Support input_re and output_sub parameters

--- src/hatch_msgfmt/plugin.py
+++ src/hatch_msgfmt/plugin.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any, Mapping
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from pathlib import Path
@@ -18,13 +19,18 @@ class MsgfmtBuildHook(BuildHookInterface):
         root = Path(self.root)
         locales = root / Path(self.config['locales'])
         destination = Path(self.config['destination'])
+        input_re = self.config['input_re']
+        output_sub = self.config['output_sub']
 
         for po in locales.glob('**/*.po'):
-            dest = destination / po.relative_to(locales)
+            mo = po.relative_to(locales)
+            if input_re:
+                mo = Path(re.sub(input_re, output_sub, str(mo)))
+            dest = str(destination / mo)
 
             output = UnopenedTemporaryFile(suffix='.mo')
 
             run(['msgfmt', '-o', str(output), str(po)], check=True, stdin=DEVNULL)
 
             self.__files.append(output)
-            build_data['force_include'][str(output)] = str(dest)
+            build_data['force_include'][str(output)] = dest
